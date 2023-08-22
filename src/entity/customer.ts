d71d138aa05c07193ebcda05c49bc0b2290ca465
Filipe Cavalcante <filipe.cavalcante@grancursosonline.com.br>
2023-08-22 17:11:20 -0300

    principio autovalidação

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -3,11 +3,25 @@ class Customer {
     _id: string;
     _name: string;
     _address: string;
+    _active: boolean = false;
 
     constructor(id: string, name: string, address: string) {
         this._id = id;
         this._name = name;
         this._address = address;
+        this.validate();
+    }
+
+    validate() {
+        if (this._id.length === 0) {
+            throw new Error('Id is required');
+        }
+        if (this._name.length === 0) {
+            throw new Error('Name is required');
+        }
+        if (this._address.length === 0) {
+            throw new Error('Address is required');
+        }
     }
 
     get id(): string {
@@ -22,15 +36,15 @@ class Customer {
         return this._address;
     }
 
-    set id(id: string) {
-        this._id = id;
-    }
-
-    set name(name: string) {
+    changeName(name: string) {
         this._name = name;
+        this.validate();
     }
 
-    set address(address: string) {
-        this._address = address;
+    activate() {
+        if (this._address.length === 0) {
+            throw new Error('Adrress is mandatory to activate customer');
+        }
+        this._active = true;
     }
 }
